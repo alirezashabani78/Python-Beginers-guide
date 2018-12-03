bf9545b251fddad67e92d479b6abf55c945484bf
AdamLundell1992 <u1853373>
2018-12-03 17:28:15 +0000

    new

--- week7/week7q6.1.py
+++ week7/week7q6.1.py
@@ -18,6 +18,10 @@ def random_string_1(b):
     return random.choice(b)
 def str_in_str(word, question):
     return word.lower() in question.lower()
+def random_shutdown():
+    num =random.randint(0,101)
+    if num < 15:
+        return True
 email = input(str("type a email adress "))
 domain = "pop.ac.uk"
 if email_address(email, domain) is True:
@@ -25,44 +29,27 @@ if email_address(email, domain) is True:
     a = ["joe", "Gary", "Steven", "Tony", "Adam", "Linda"]
     print("Hello, my name is " + random_string(a) + ". i would be helping you today")
     count = 1
-    while count <= 3:
+    while True:
         question = str(input("hello " + name_part(email) + "," " how can i help you? "))
         if "goodbye" in question.lower():
             print("exit")
             break
+        if random_shutdown():
+            print("connection lost,")
+            break
         elif "library" in question.lower():
             print("Sorry the library is closed today")
-            count += 1
-            if count == 4:
-                print("connection has been lost")
         elif "i need coffee" in question.lower():
             print("yes you doo")
-            count += 1
-            if count == 4:
-                print("connection has been lost")
         elif "wifi" in question.lower():
             print("wifi connection is great across campus")
-            count += 1
-            if count == 4:
-                print("connection has been lost")
         elif "deadline" in question.lower():
             print("Sorry, your deadline has been extended 2 working days")
-            count += 1
-            if count == 4:
-                print("connection has been lost")
         elif question:
             b = ["Yes", "No", "Sounds interesting, tell me more", "haha! wow that was sick! tell me more", "maybe"]
             print("" + random_string_1(b) + "")
-            count += 1
-            if count == 4:
-                print("connection has been lost")
-                break
         else:
             print("Error")
-
-
-
-
 
 
 
